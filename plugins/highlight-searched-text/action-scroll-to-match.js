a7108de5f6a8f0577b6480818b41311720c16e62
bimlas <bimbalaszlo@gmail.com>
2019-11-14 14:27:54 +0100

    Refactoring

--- plugins/highlight-searched-text/action-scroll-to-match.js
+++ plugins/highlight-searched-text/action-scroll-to-match.js
@@ -56,47 +56,53 @@ Action widget to scroll to the next match in story river.
 	Invoke the action associated with this widget
 	*/
 	ScrollToMatch.prototype.invokeAction = function(triggeringWidget,event) {
-		var self = this;
 		var isHighlightingUpdated = updateHighlighting();
 		var allMatches = $tw.pageContainer.querySelectorAll('mark');
 		if(allMatches.length === 0) return true;
 		if(isHighlightingUpdated && resultsAreFoundOnTheCurrentScreen()) return true;
 
-		var indexOfClosestMatch = this.actionDirection === "previous"
+		var targetMatch = this.actionDirection === "previous"
 			? findBackwardFromThePreviousHalfScreen()
 			: findForwardFromTheNextHalfScreen();
 
-		$tw.pageScroller.scrollIntoView(allMatches[indexOfClosestMatch], function() {
-			var boundingBox = allMatches[indexOfClosestMatch].getBoundingClientRect();
-			boundingBox.y -= window.innerHeight / 2;
-			return boundingBox;
+		$tw.pageScroller.scrollIntoView(targetMatch, function() {
+			return alignBoundingBoxToMiddleOfTheScreen(targetMatch.getBoundingClientRect());
 		});
 		return true; // Action was invoked
 
 		function resultsAreFoundOnTheCurrentScreen() {
 			return Array.from(allMatches).reduce(function(accumulator,match) {
-				var elementYOffset = match.getBoundingClientRect().y;
-				return accumulator || (elementYOffset >= 0 && elementYOffset <= window.innerHeight);
+				return accumulator || (getPositionOfElementRelativeToScreen(match) === 0);
 			}, false);
 		}
 
 		function findBackwardFromThePreviousHalfScreen() {
 			for(var index = allMatches.length-1; index >= 0; index--) {
-				var elementYOffset = allMatches[index].getBoundingClientRect().y;
-				if(elementYOffset <= 0) {
-					return index;
+				if(getPositionOfElementRelativeToScreen(allMatches[index]) < 0) {
+					return allMatches[index];
 				}
 			}
-			return allMatches.length-1;
+			return allMatches[allMatches.length-1];
 		}
 
 		function findForwardFromTheNextHalfScreen() {
 			for(var index = 0; index < allMatches.length; index++) {
-				var elementYOffset = allMatches[index].getBoundingClientRect().y;
-				if(elementYOffset >= window.innerHeight) {
-					return index;
+				if(getPositionOfElementRelativeToScreen(allMatches[index]) > 0) {
+					return allMatches[index];
 				}
 			}
+			return allMatches[0];
+		}
+
+		function alignBoundingBoxToMiddleOfTheScreen(boundingBox) {
+			boundingBox.y -= window.innerHeight / 2;
+			return boundingBox;
+		}
+
+		function getPositionOfElementRelativeToScreen(elem) {
+			var elementYOffset = elem.getBoundingClientRect().y;
+			if (elementYOffset < 0) return -1;
+			if (elementYOffset > window.innerHeight - 30 /* height of the toolbar */) return 1;
 			return 0;
 		}
 	};
